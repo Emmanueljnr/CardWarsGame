8d1a3d82a5d0999d045d30324c1859c975bc6f11
emmanuel <emmanuel@emmanuel-IdeaPad-5-14ITL05>
2023-08-24 23:51:50 +0100

    Fixed the game play logic in the main method, and I have now allowed the game to house more than 2 players. Players also have the ability to chose who they would like to attack.

--- src/main/java/com/emmanuelrufasha/pocketbeastscardgame/Main.java
+++ src/main/java/com/emmanuelrufasha/pocketbeastscardgame/Main.java
@@ -5,6 +5,7 @@ import interfaces.ICard;
 import interfaces.IPlayerObserver;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 
@@ -71,33 +72,15 @@ public class Main {
         // Prompt for the number of players       
         int numberOfPlayersParticipating;      
         Scanner scanner = new Scanner(System.in);
-//        while (true){
-//            System.out.println("> How many players will be participating in this game ? : "); 
-//            if (scanner.hasNextInt()){
-//                numberOfPlayersParticipating = scanner.nextInt();
-//                System.out.println(); // This line will print an empty line
-//                if (numberOfPlayersParticipating > 1){
-//                    break; //valid input, exit the loop here
-//                }
-//                else {
-//                    scanner.next();//clear invalid input
-//                }
-//            } else 
-//            {
-//                 System.out.println("> Invalid input! Please enter a digit greater than 1.");
-//                 scanner.next();//clear invalid input
-//            }
-//            
-//        }  
-//        scanner.nextLine();
-        
+
+        // Handle multiple players : 
         
         System.out.println(); // This line will print an empty line
         while (true) {
             System.out.println("> How many players will be participating in this game ? : ");
             if (scanner.hasNextInt()) {
                 numberOfPlayersParticipating = scanner.nextInt();
-                System.out.println(""); // This line will print an empty line
+//                System.out.println(""); // This line will print an empty line
                 if (numberOfPlayersParticipating > 1) {
                     break; // valid input, exit the loop here
                 } else {
@@ -115,16 +98,16 @@ public class Main {
         
         // Create a new instance of the DeckFactory
         DeckFactory deckFactory = new DeckFactory(); 
-        
+        //ADD PLAYERS FUBCTION IN THE PLAYERS CLASS*********************************************************************************************************************
         //Collection of all players playing.
         List<Player> players = new ArrayList<>();
         
         //Based on how many players playing, ask for their individual names. 
         for (int i = 1; i <= numberOfPlayersParticipating; i++) {
             while (true) {
-                System.out.println("> Player " + i + ", enter your name: ");
-                String playerName = scanner.nextLine();
-                System.out.println(); // This line will print an empty line
+                System.out.println("\n> Player " + i + ", enter your name: ");
+                String playerName = scanner.next();
+                //System.out.println(); // This line will print an empty line
                 // Check if the name is valid using a regular expression
                 if (playerName.matches("^[a-zA-Z][a-zA-Z0-9]*$")) {
                     players.add(new Player(playerName, deckFactory.createNewDeck()));
@@ -132,12 +115,20 @@ public class Main {
                 } 
                 System.out.println("> INVALID INPUT! A Name cannot start with a symbol or number and cannot contain spaces.");
             }
-        }
+        }scanner.nextLine();  // clears out the newline
         
 
         // Start the game
-        System.out.println("> LET THE GAMES BEGIN... \n  Good Luck!");  
-        System.out.println("\n"); // Print an empty line
+        System.out.println("\n> LET THE GAMES BEGIN... \n  Good Luck!");  
+        //System.out.println("\n"); // Print an empty line
+        
+        
+        System.out.println("\n>  Press ENTER to start the game...");
+        Scanner start = new Scanner(System.in);
+        start.nextLine();
+        
+        
+        
         System.out.println(" ---------------------------------------------------------------------- Initial Game Board ----------------------------------------------------------------------  \n\n"); 
         for (Player player : players) {
             player.newGame();
@@ -145,124 +136,173 @@ public class Main {
         }
         System.out.println("\n ----------------------------------------------------------------------------------------------------------------------------------------------------------------  \n\n");
         
+// MAIN METHOD SHOULD CREATE THE GAME CLASS THEN RUN IT
+//ADD THE LOOP BELOW TO QA GAMELOGIC CLASS 
 
+    // Create an instance of the AttackStrategy
+    IAttackStrategy attackStrategy = new AttackStrategy();
 
-        // Create an instance of the AttackStrategy
-        IAttackStrategy attackStrategy = new AttackStrategy();
-          
-        String winningMessage = "";
-        Boolean run = true;
-        while(run) {
+    String winningMessage = "";
+    Boolean run = true;
+    while(run) {
         for (Player player : players) {
             // Add mana and draw card
             player.addMana();
             player.drawCard();
-            System.out.println(player); 
+            System.out.println(player); // display cards
+            
+//            System.out.println(player.getName() + ", it's your turn! \n"); 
 
-            // Hack assumes only one other player
-            Player otherPlayer = null;
-            for (Player iPlayer : players) {
-                if (iPlayer != player) {
-                    otherPlayer = iPlayer;
-                }
-            }
-            if (otherPlayer == null){
+
+            // Get a list of all potential targets (other players)
+            List<Player> potentialTargets = new ArrayList<>(players);
+            potentialTargets.remove(player); // remove the current player from the list of targets
+
+            if (potentialTargets.isEmpty()) {
                 winningMessage = "Something has gone terribly wrong...";
                 run = false;
                 break;
             }
-                
-                // Cycle through cards in play to attack
-                for (ICard card : player.getInPlay().getCards()) {
-                    System.out.println(card.toString());
 
-                    String attack = getPrompt(
-                            player.getName() + " attack with " + card.getName() + "? (Yes/No): ", 
-                            new String[]{"Yes", "yes", "y", "No", "no", "n"}); //CHECK FOR VALID INPUTS
-                    if (attack.equals("Yes") || attack.equals("yes") || attack.equals("y")) {
-                        // Choose who to attack, player directly or a player's beast
-                        int attackChoice = 2;
-                        System.out.println("Who would you like to attack? ");
-                        System.out.println("1. " + otherPlayer.getName());
-                        for (ICard otherCard: otherPlayer.getInPlay().getCards()) {
-                            System.out.println(attackChoice + ". " + otherCard);
-                            attackChoice++;
-                        }
-                        ArrayList<String> prompts = new ArrayList<>();
-                        for (int i=1; i<attackChoice; i++) {
-                            prompts.add(String.valueOf(i));
-                        }
-                        String target = getPrompt("Choose a number: ", prompts.toArray(new String[0]));
-                        // if Target is "1" then we attack the player directly...
-                        if (target.equals("1")) { 
-                            attackStrategy.performAttack(player, otherPlayer, card);
-                            if (otherPlayer.damage(card.getAttack())) {
-                                // if true returned players health <= 0
-                                winningMessage = player.getName() + " wins!";
-                                run = false;
-                                break;
-                            }
-                            //System.out.println(otherPlayer.getName() + " is now at " + otherPlayer.getHealth());                        
-                            // Notify observers about health change
-                            for (IPlayerObserver observer : otherPlayer.getObservers()) {
-                                observer.onHealthChange(otherPlayer.getName(), otherPlayer.getHealth());
-                            }
-                        }
-                        // Else if Target is "2" then we attack the beast...
-                        else {                          
-                            ICard targetCard = otherPlayer.getInPlay().getCard(Integer.parseInt(target)- 2);
-                            attackStrategy.performAttack(player, otherPlayer, card, targetCard);
-                        }
-                    }
-                }
+            
+            
+        // Loop through each player to play cards from their hand
+        for (Player currentPlayer : players) {
+            ArrayList<ICard> toRemove = new ArrayList<>();
+            for (ICard card : currentPlayer.getHand().getCards()) {
+                if (card.getManaCost() <= currentPlayer.getManaAvailable()) {         
                 
-                if (!run) {
-                    break;
-                }
-                
-                // Cycle through cards in play remove "dead" cards (health <= 0)
-                ArrayList<ICard> toRemove = new ArrayList<>();
-                for (ICard card : player.getInPlay().getCards()) {
-                    if (card.getHealth() <= 0) {
-                        toRemove.add(card);
-                        player.getGraveyard().add(card);
-                    }
-                }
-                player.getInPlay().removeAll(toRemove);
-                
-                toRemove = new ArrayList<>();
-                for (ICard card : otherPlayer.getInPlay().getCards()) {
-                    if (card.getHealth() <= 0) {
-                        toRemove.add(card);
-                        otherPlayer.getGraveyard().add(card);
-                    }
-                }
-                otherPlayer.getInPlay().removeAll(toRemove);
+                System.out.println("> " + player.getName() + ", it's your turn! \n");
+                System.out.println(card.toString() + "\n\n> " + currentPlayer.getName() + " play " + card.getName() + "? (Yes/No): ");
+                String play = scanner.nextLine().trim().toLowerCase();
 
-                // Play cards from hand
-                toRemove = new ArrayList<>();
-                for (ICard card : player.getHand().getCards()) {
-                    if (card.getManaCost() <= player.getManaAvailable()) {
-                        System.out.println(card.toString());
-
-                        String play = getPrompt(
-                                player.getName() + " play " + card.getName() + "? (Yes/No) ", 
-                                new String[]{"Yes", "yes", "y", "No", "no", "n"});
-                        if (play.equals("Yes") || play.equals("yes") || play.equals("y")) {
-                            player.getInPlay().add(card);
-                            player.useMana(card.getManaCost());
-                            toRemove.add(card);
-                        }
-                    }
+                // Validate the input:
+                while (!Arrays.asList("yes", "y", "no", "n").contains(play)) {
+                    System.out.println("Invalid input. Please enter Yes or No.");
+                    System.out.println("\n" + currentPlayer.getName() + ", would you like to play " + card.getName() + "? (Yes/No): ");
+                    play = scanner.nextLine().trim().toLowerCase();
                 }
-                player.getHand().removeAll(toRemove);
-                
-                // Print final play state
-                System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
-                System.out.println(player);
+
+                if (play.equals("yes") || play.equals("y")) {
+                    currentPlayer.getInPlay().add(card);
+                    currentPlayer.useMana(card.getManaCost());
+                    toRemove.add(card);
+                }
+           
+                    
+                }
             }
+            currentPlayer.getHand().removeAll(toRemove);
+        }
+            
+            
+            
+            
+            
+            // Attack Loop
+            for (ICard card : player.getInPlay().getCards()) {
+                //System.out.println(card.toString() + "\n"); //print out cards
+
+
+                System.out.println("\n" + card.toString() + "\n\n> "  + player.getName() + " attack with " + card.getName() + "? (Yes/No): ");
+                String attack = scanner.nextLine().trim().toLowerCase();
+
+                // Now you might want to validate the input:
+                while (!Arrays.asList("yes", "y", "no", "n").contains(attack)) {
+                    System.out.println("Invalid input. Please enter Yes or No.");
+                    System.out.println(player.getName() + " attack with " + card.getName() + "? (Yes/No): ");
+                    attack = scanner.nextLine().trim().toLowerCase();
+                }
+
+                if (attack.equalsIgnoreCase("Yes") || attack.equalsIgnoreCase("y")) {
+                    // Choose a target player to attack
+                    System.out.println("\n" + player.getName() + ", choose a player to attack:");
+
+                    int selectedPlayerIndex = -1;
+                    while (selectedPlayerIndex < 1 || selectedPlayerIndex > potentialTargets.size()) {
+                        for (int i = 0; i < potentialTargets.size(); i++) {
+                            System.out.println((i + 1) + ". " + potentialTargets.get(i).getName());
+                        }
+                        selectedPlayerIndex = scanner.nextInt(); 
+                        scanner.nextLine();
+                        if (selectedPlayerIndex < 1 || selectedPlayerIndex > potentialTargets.size()) {
+                            System.out.println("Invalid choice. Please enter a valid number between 1 and " + potentialTargets.size() + ".");
+                        } 
+                    }
+
+                    Player targetPlayer = potentialTargets.get(selectedPlayerIndex - 1);
+                    System.out.println("\n" + player.getName() + ", you chose to attack " + targetPlayer.getName() + ". \n \n");
+
+                    // Decide whether to attack the target player directly or one of their beasts
+                    System.out.println("Which player would you like to attack? ");
+                    System.out.println("1. " + targetPlayer.getName());
+
+                    List<String> prompts = new ArrayList<>();
+                    prompts.add("1");
+                    int attackChoice = 2;
+                    for (ICard otherCard : targetPlayer.getInPlay().getCards()) {
+                        System.out.println(attackChoice + ". " + otherCard);
+                        prompts.add(String.valueOf(attackChoice));
+                        attackChoice++;
+                    }
+
+                    String target = getPrompt("Choose a number: ", prompts.toArray(new String[0]));
+
+                    // If Target is "1" then we attack the player directly...
+                    if (target.equals("1")) { 
+                        attackStrategy.performAttack(player, targetPlayer, card);
+                        if (targetPlayer.damage(card.getAttack())) {
+                            winningMessage = player.getName() + " wins!";
+                            run = false;
+                            break;
+                        }
+                        for (IPlayerObserver observer : targetPlayer.getObservers()) {
+                            observer.onHealthChange(targetPlayer.getName(), targetPlayer.getHealth());
+                        }
+                    }
+                    // Else if Target is "2" or higher then we attack the selected beast...
+                    else {
+                        ICard targetCard = targetPlayer.getInPlay().getCard(Integer.parseInt(target) - 2);
+                        attackStrategy.performAttack(player, targetPlayer, card, targetCard);
+                    }
+                }
+            }
+        if (!run) {
+            break;
+        }
+
+
+        // Cycle through all players' cards and remove "dead" cards (health <= 0)
+        for (Player currentPlayer : players) {
+            ArrayList<ICard> toRemove = new ArrayList<>();
+            for (ICard card : currentPlayer.getInPlay().getCards()) {
+                if (card.getHealth() <= 0) {
+                    toRemove.add(card);
+                    currentPlayer.getGraveyard().add(card);
+                }
+            }
+            currentPlayer.getInPlay().removeAll(toRemove);
+        }
+
+        // Print final play state for each player
+        System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+        for (Player currentPlayer : players) {
+            System.out.println(currentPlayer);
+        }
+
+        }
         }
         
         System.out.println(winningMessage);
     }
 }
+
+
+
+
+                // Cycle through cards in play to attack - Attack LOOP
+                //Print out all the cards in play for me ]
+                //print out all the cards that are still in play for all the other players
+                //ask current player, would you like to attack
+                //what would you like to attack with
+                //who would you like to attack?
